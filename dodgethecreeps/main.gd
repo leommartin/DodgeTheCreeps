0d7fdcc0678d940dbcb1a57f42424b08f5f6edae
Leonardo Martin <leowork2002@gmail.com>
2025-01-04 19:36:59 -0400

    inserindo labels e botoes a tela inicial do jogo

--- dodgethecreeps/main.gd
+++ dodgethecreeps/main.gd
@@ -4,7 +4,7 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,15 +13,18 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
 func _on_score_timer_timeout() -> void:
 	score +=1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
