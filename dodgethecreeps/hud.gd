760e36805b5cf1a6f2e336c1f224bdd7537cbae2
Leonardo Martin <leowork2002@gmail.com>
2025-01-06 10:12:41 -0400

    adicionando rastro do jogador

--- dodgethecreeps/hud.gd
+++ dodgethecreeps/hud.gd
@@ -33,12 +33,10 @@ func show_game_over():
 	
 func update_score(score):
 	$ScoreLabel.text = str(score)
-
-
+	
 func _on_start_button_pressed() -> void:
 	$StartButton.hide()
 	start_game.emit()
 	
-
 func _on_message_timer_timeout() -> void:
 	$Message.hide()
